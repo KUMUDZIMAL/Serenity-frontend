b641eded8587ac1bc7795bd693793884a829e57b
KUMUDZIMAL <kumud.223133206@vcet.edu.in>
2025-07-23 15:40:38 -0700

    done

--- middleware.ts
+++ middleware.ts
@@ -24,5 +24,5 @@ export async function middleware(request: NextRequest) {
 
 // Protect specific routes
 export const config = {
-  matcher: ['/dashboard', '/Input1', '/Input2','/journel','/community'], // Protect these routes
+  matcher: ['/dashboard', '/Input1', '/Input2'], // Protect these routes
 };
